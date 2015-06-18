394038cf407dbff9dcff573131e8c568f2869f67
othmanrefat <ref3tothman@gmail.com>
2015-06-18 13:39:43 +0300

    fist commit

--- client/app/registeration/registeration.controller.js
+++ client/app/registeration/registeration.controller.js
@@ -1,25 +1,45 @@
 'use strict';
 
+// angular.module('doorbellApp')
+//   .controller('RegisterationCtrl', function ($scope, $firebase) {
+//     $scope.message = 'Hello';
+
+
+//     //connect to firebase
+//  var ref = new Firebase("https://doorbellyamsafer.firebaseio.com/emplyee");  
+//  $scope.emplyee = $firebase(ref);
+
+
+//  $scope.add = function(){
+//  	console.log("clicked !");
+//  $scope.emplyee.$add({
+//      name : $scope.name,
+//      email : $scope.email,
+//      phoneNumber : $scope.phone
+//  });
+
+//  }
+
+
+
+//   }); 
+
+//create our angular module and anject firebase 
 angular.module('doorbellApp')
-  .controller('RegisterationCtrl', function ($scope, $firebase) {
-    $scope.message = 'Hello';
 
+//create our main controller and get access to firebase
+ .controller('RegisterationCtrl', function ($scope , $firebase) {
 
-    //connect to firebase
- var ref = new Firebase("https://doorbellyamsafer.firebaseio.com/emplyee");  
- $scope.emplyee = $firebase(ref);
-
+//connect to firebase
+ $scope.ref = new Firebase("https://doorbellyamsafer.firebaseio.com/emplyee");  
 
  $scope.add = function(){
- 	console.log("clicked !");
- $scope.emplyee.$add({
-     name : $scope.name,
-     email : $scope.email,
-     phoneNumber : $scope.phone
+ $scope.ref.push({
+     name :$scope.name,
+     email: $scope.email,
+     phone: $scope.phone
+
  });
 
- }
-
-
-
-  });
+};
+});
