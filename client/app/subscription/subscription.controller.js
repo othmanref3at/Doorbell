f8abb32f30162eb50ca16e1c96b2aa5753011f73
othmanrefat <ref3tothman@gmail.com>
2015-06-23 13:56:43 +0300

    change controller

--- client/app/subscription/subscription.controller.js
+++ client/app/subscription/subscription.controller.js
@@ -37,61 +37,62 @@ angular.module('doorbellApp')
 			// 		});
 			// 	}
 			// });
-function notify(e){
-	var notifi;
-if(Notification.permission === 'default'){
-	alert('Please allow notifications before doing this');
+		function notify(e) {
+			var notifi;
+			if (Notification.permission === 'default') {
+				alert('Please allow notifications before doing this');
 
-}else{
- 	notifi= new Notification('New message from the door',{
- 		body: 'Please go open the door',
- 		icon: './../../assets/images/message.png'
-
- 		 
- 	});
- 	notifi.onclick = function(){
- 		alert("Im comming :P");
-
- 	}
-}
-}
-  console.log("SW presentddddd !!! ");
- //    if ('serviceWorker' in navigator){
- //    console.log("SW present !!! ");
-
- //    navigator.serviceWorker.register('worker.js', {
- //      //scope: '/toto/'
- //    }).then(function(registration){
- //      console.log('Service worker registered : ', registration.scope);
- //    })
- //    .catch(function(err){
- //      console.log("Service worker registration failed : ", err);
- //    });
-
- //  }
+			} else {
+				notifi = new Notification('New message from the door', {
+					body: 'Please go open the door',
+					icon: './../../assets/images/message.png'
 
 
+				});
+				notifi.onclick = function() {
+					alert("Im comming :P");
 
-// https://example.com/webapp.js
-// navigator.serviceWorker.register('./serviceworker.js').then(
-//   function(serviceWorkerRegistration) {
-//     serviceWorkerRegistration.pushManager.subscribe().then(
-//       function(pushSubscription) {
-//         console.log(pushSubscription.endpoint);
-//         // The push subscription details needed by the application
-//         // server are now available, and can be sent to it using,
-//         // for example, an XMLHttpRequest.
-//       }, function(error) {
-//         // During development it often helps to log errors to the
-//         // console. In a production environment it might make sense to
-//         // also report information about errors back to the
-//         // application server.
-//         console.log(error);
-//       }
-//     );
-//   }
+				}
+			}
+		}
+		console.log("SW presentddddd !!! ");
+		if ('serviceWorker' in navigator) {
+			console.log("SW present !!! ");
 
-//   );
+			navigator.serviceWorker.register('serviceworker.js', {
+					console.log("refa");
+					//scope: '/toto/'
+				}).then(function(registration) {
+					console.log('Service worker registered : ', registration.scope);
+				})
+				.catch(function(err) {
+					console.log("Service worker registration failed : ", err);
+				});
+
+		}
+
+
+
+		// https://example.com/webapp.js
+		// navigator.serviceWorker.register('./serviceworker.js').then(
+		//   function(serviceWorkerRegistration) {
+		//     serviceWorkerRegistration.pushManager.subscribe().then(
+		//       function(pushSubscription) {
+		//         console.log(pushSubscription.endpoint);
+		//         // The push subscription details needed by the application
+		//         // server are now available, and can be sent to it using,
+		//         // for example, an XMLHttpRequest.
+		//       }, function(error) {
+		//         // During development it often helps to log errors to the
+		//         // console. In a production environment it might make sense to
+		//         // also report information about errors back to the
+		//         // application server.
+		//         console.log(error);
+		//       }
+		//     );
+		//   }
+
+		//   );
 
 
 	});
