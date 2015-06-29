c08132de79bb438c626517ad319168cc725807ad
saja-Kharosha <1110710@student.birziet.edu>
2015-06-29 16:07:31 +0300

    creat repo

--- client/app/registeration/registeration.controller.js
+++ client/app/registeration/registeration.controller.js
@@ -9,11 +9,34 @@ angular.module('doorbellApp')
 	//connect to firebase
 	$scope.ref = new Firebase("https://doorbellyamsafer.firebaseio.com/emplyee");
 	$scope.add = function() {
-		$scope.ref.push({
+		$scope.ref.child($scope.phone).set({
 			name: $scope.name,
 			email: $scope.email,
 			phone: $scope.phone
 
 		});
+	};
+
+
+		$scope.retrive=function($locationProvider){
+		$scope.ref.orderByValue().limitToLast(2).on('value',function(snapshot) {
+			
+			$rootScope.currentUser = snapshot.val();
+			
+			
+		});
+		console.log($rootScope.currentUser);
+			console.log(typeof ($rootScope.currentUser));
+			
+			if($rootScope.currentUser ==='undefined'){
+				console.log("undefined rootScope");
+			}else {
+				console.log("defined rootScope");
+				//$state.go('test22');
+			}
+
+			return $rootScope.currentUser;
+
+		
 	};
 });
--- server/api/register/index.js
+++ server/api/register/index.js
@@ -5,6 +5,6 @@ var controller = require('./register.controller');
 
 var router = express.Router();
 
-router.post('/', controller.index);
+//router.post('/', controller.index);
 
 module.exports = router;
